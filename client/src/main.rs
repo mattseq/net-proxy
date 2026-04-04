af69f1fd761903ee1d5ec86de3fbebe1ee94a9e8
mattseq <matthewsequeir@gmail.com>
2026-04-04 15:12:29 -0500

    docker testing environment and fixes
    tested and working

--- client/src/main.rs
+++ client/src/main.rs
@@ -24,18 +24,22 @@ impl Drop for CleanUp {
         std::process::Command::new("ip")
             .args(["route", "del", "0.0.0.0/0", "dev", "tun0"])
             .status().ok();
+        std::process::Command::new("ip")
+            .args(["route", "add", "default", "via", &self.gateway_ip])
+            .status().ok();
     }
 }
 
 fn main() {
-    const VPS_IP: &str = "24.55.14.254";
+    const VPS_IP: &str = "172.20.0.10";
     const VPS_PORT: &str = "5000";
-    const GATEWAY_IP: &str = "192.168.1.1";
+    const GATEWAY_IP: &str = "172.20.0.1";
 
     let mut config = tun::Configuration::default();
     config
         .address("10.0.0.1")
         .netmask("255.255.255.255")
+        .destination("10.0.0.2")
         .up();
 
     let device = tun::create(&config).unwrap();
@@ -47,11 +51,18 @@ fn main() {
         .args(["route", "add", &vps_route, "via", GATEWAY_IP])
         .status().unwrap();
 
-    // route all other traffic to tun0
     std::process::Command::new("ip")
-        .args(["route", "add", "0.0.0.0/0", "dev", "tun0"])
+        .args(["route", "del", "default"])
         .status()
         .unwrap();
+
+    // route all other traffic to tun0
+    std::process::Command::new("ip")
+        .args(["route", "add", "default", "dev", "tun0"])
+        .status()
+        .unwrap();
+
+    println!("routes set");
 
     // cleanup Drop trait handles removing tun0 and ip routes
     let _cleanup = CleanUp::new(VPS_IP, GATEWAY_IP);
@@ -65,23 +76,40 @@ fn main() {
     // split tun device into reader and writer for separate threads
     let (mut reader, mut writer) = device.split();
 
+    let v_ip = VPS_IP.to_string();
+    let g_ip = GATEWAY_IP.to_string();
     ctrlc::set_handler(move || {
+        let route = format!("{}/32", v_ip);
+
+        std::process::Command::new("ip")
+            .args(["route", "del", &route, "via", &g_ip])
+            .status().ok();
+        std::process::Command::new("ip")
+            .args(["route", "del", "0.0.0.0/0", "dev", "tun0"])
+            .status().ok();
+        std::process::Command::new("ip")
+            .args(["route", "add", "default", "via", &g_ip])
+            .status().ok();
+
         std::process::exit(0);
     }).unwrap();
 
     // receive thread: receive from udp and write to device
     std::thread::spawn(move || {
-        let mut buf = vec![0u8; 1500];
+        let mut buf = vec![0u8; 1528];
         loop {
             let (n, src) = udp_recv.recv_from(&mut buf).unwrap();
+            println!("packet returned from {}", src);
+
             writer.write_all(&buf[..n]).unwrap();
         }
     });
 
     // write thread: read from device and write to udp
-    let mut buf = vec![0u8; 1500];
+    let mut buf = vec![0u8; 1528];
     loop {
         let n = reader.read(&mut buf).unwrap();
         udp.send_to(&buf[..n], &vps_addr).unwrap();
+        println!("packet sent to {}", &vps_addr);
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -6,7 +6,6 @@ struct CleanUp;
 
 impl Drop for CleanUp {
     fn drop(&mut self) {
-
         std::process::Command::new("sysctl")
             .args(["-w", "net.ipv4.ip_forward=0"])
             .status()
@@ -16,6 +15,13 @@ impl Drop for CleanUp {
             .args(["-t", "nat", "-D", "POSTROUTING", "-o", "eth0", "-j", "MASQUERADE"])
             .status()
             .unwrap();
+
+        std::process::Command::new("iptables")
+            .args(["-D", "FORWARD", "-i", "tun0", "-o", "eth0", "-j", "ACCEPT"])
+            .status().ok();
+        std::process::Command::new("iptables")
+            .args(["-D", "FORWARD", "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT"])
+            .status().ok();
     }
 }
 
@@ -24,24 +30,58 @@ fn main() {
     let mut config = tun::Configuration::default();
     config
         .address("10.0.0.2")
-        .netmask("255.255.255.0")
+        .netmask("255.255.255.255")
+        .destination("10.0.0.1")
         .up();
 
     let device = tun::create(&config).unwrap();
 
+    // route new packets in tun0 to their destination instead of ignoring bc different destination
     std::process::Command::new("sysctl")
         .args(["-w", "net.ipv4.ip_forward=1"])
         .status()
         .unwrap();
 
+    // packets that jump from tun0 to eth0 should have their src rewritten to this vps ip
     std::process::Command::new("iptables")
         .args(["-t", "nat", "-A", "POSTROUTING", "-o", "eth0", "-j", "MASQUERADE"])
         .status()
         .unwrap();
 
+    // tun0 -> eth0; let packets written to tun0 jump to eth0
+    std::process::Command::new("iptables")
+        .args(["-A", "FORWARD", "-i", "tun0", "-o", "eth0", "-j", "ACCEPT"])
+        .status().ok();
+
+    // eth0 -> tun0; only allow packets from established connection to jump from eth0 to tun0
+    std::process::Command::new("iptables")
+        .args(["-A", "FORWARD", "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT"])
+        .status().ok();
+
+    println!("routes set");
+
     let _cleanup = CleanUp;
 
-    ctrlc::set_handler(|| std::process::exit(0)).unwrap();
+    ctrlc::set_handler(move || {
+        std::process::Command::new("sysctl")
+            .args(["-w", "net.ipv4.ip_forward=0"])
+            .status()
+            .unwrap();
+
+        std::process::Command::new("iptables")
+            .args(["-t", "nat", "-D", "POSTROUTING", "-o", "eth0", "-j", "MASQUERADE"])
+            .status()
+            .unwrap();
+
+        std::process::Command::new("iptables")
+            .args(["-D", "FORWARD", "-i", "tun0", "-o", "eth0", "-j", "ACCEPT"])
+            .status().ok();
+        std::process::Command::new("iptables")
+            .args(["-D", "FORWARD", "-m", "state", "--state", "ESTABLISHED,RELATED", "-j", "ACCEPT"])
+            .status().ok();
+
+        std::process::exit(0);
+    }).unwrap();
 
     let client_addr: Arc<Mutex<Option<SocketAddr>>> = Arc::new(Mutex::new(None));
     let client_addr_write = Arc::clone(&client_addr);
@@ -52,23 +92,24 @@ fn main() {
 
     let (mut reader, mut writer) = device.split();
 
-    // receive thread: receive from client, modify sender ip, and send through udp
+    // proxy thread: receive from client through udp, modify sender ip (nat rule), and send through tun
     std::thread::spawn(move || {
-        let mut buf = vec![0u8; 1500];
+        let mut buf = vec![0u8; 1528];
         loop {
             let (n, src) = udp_recv.recv_from(&mut buf).unwrap();
-            // src is still 192.168.x.x (local ip of sender), need to replace with LAN_IP
             *client_addr_write.lock().unwrap() = Some(src);
             writer.write_all(&buf[..n]).unwrap();
+            println!("packet forwarded (client src={})", src)
         }
     });
 
-    // send back thread: receive from udp, modify destination ip, send back through udp
-    let mut buf = vec![0u8; 1500];
+    // receive thread: receive from tun, modify destination ip (nat rule), send back to client through udp
+    let mut buf = vec![0u8; 1528];
     loop {
         let n = reader.read(&mut buf).unwrap();
         if let Some(addr) = *client_addr_read.lock().unwrap() {
             udp.send_to(&buf[..n], addr).unwrap();
+            println!("packet received, backward to og src={}", addr);
         }
     }
 }
